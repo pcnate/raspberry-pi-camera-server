59228d3b79a2d9a7813a7d70aaf6eba440b8908a
Nathan Baker <pcnate@gmail.com>
2018-10-02 03:20:40 -0500

    refresh images, listen for uploads and trigger a refresh

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 var express = require('express');
+const fs = require('fs');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+const EventEmmitter = require("events");
+
+app.use( bodyParser.json() );
+app.enable( 'trust proxy' );
+
+class ServerEmitter extends EventEmmitter { }
+const serverEmitter = new ServerEmitter();
+
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -9,6 +21,57 @@ io.on('connection', function(socket){
 
 app.get('/', function( req, res ) {
   res.sendFile("www/index.html", { root: __dirname });
+});
+
+var cam1, cam2, cam3, cam4;
+
+app.get('/cam1.jpg', ( request, response ) => {
+  console.log('cam1' );
+  response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+  response.write( cam1 || '' );
+  response.end();  
+})
+
+app.get('/cam2.jpg', ( request, response ) => {
+  console.log('cam2' );
+  response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+  response.write( cam2 || '' );
+  response.end();  
+})
+
+app.get('/cam3.jpg', ( request, response ) => {
+  console.log('cam3' );
+  response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+  response.write( cam3 || '' );
+  response.end();  
+})
+
+app.get('/cam4.jpg', ( request, response ) => {
+  console.log('cam4' );
+  response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+  response.write( cam4 || '' );
+  response.end();  
+})
+
+app.post('/upload', multipartMiddleware, async ( request, response ) => {
+  console.log('upload', request.query, request.connection.remoteAddress );
+  await fs.readFile( request.files.filedata.path, ( error, data ) => {
+
+    if( error ) {
+      console.error( 'error uploading image', error );
+      response.status( 500 ).send();
+    } else {
+
+      // if ( request.connection.remoteAddress === '::ffff:128.10.100.71' ) {
+      cam1 = data;
+      cam2 = data;
+      cam3 = data;
+      cam4 = data;
+      response.send();
+      serverEmitter.emit('imageRefresh');
+    }
+
+  });
 });
 
 http.listen( 3000, function() {
@@ -21,5 +84,10 @@ io.on('connection', function( socket ) {
 
   socket.on('disconnect', function() {
     console.log('user disconnected');
+  })
+
+  serverEmitter.on("imageRefresh", function () {
+    console.log('telling client to load new image');
+    socket.emit("imageRefresh");
   })
 })
