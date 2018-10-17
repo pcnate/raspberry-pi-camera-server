0338e12c0c9052f43ba5c83c71bcd06693ed1429
Nathan Baker <pcnate@gmail.com>
2018-10-16 19:59:37 -0500

    push final changes to saving images

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const fs = require('fs');
+const path = require('path');
+const dirent = require('fs').Dirent;
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -21,9 +23,13 @@ app.get('/', function( req, res ) {
 var images = {};
 
 app.get('/images/:deviceID', ( request, response ) => {
-  response.writeHead(200, { 'Content-Type': 'image/jpeg' });
-  response.write( images[request.params.deviceID] || '' );
-  response.end();  
+  if (images[request.params.deviceID] ) {
+    response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+    response.write( images[request.params.deviceID] || '' );
+    response.end();
+  } else {
+    response.status( 404 ).send('');
+  }
 })
 
 app.post('/upload/:deviceID', multipartMiddleware, async ( request, response ) => {
@@ -46,7 +52,7 @@ app.post('/upload/:deviceID', multipartMiddleware, async ( request, response ) =
 
 app.post('/upload/:deviceID/:timestamp', multipartMiddleware, async ( request, response ) => {
 
-  await fs.readFile( request.files.filedata.path, ( error, data ) => {
+  await fs.readFile( request.files.filedata.path, async ( error, data ) => {
 
     if( error ) {
       console.error( 'error uploading image', error );
@@ -54,13 +60,60 @@ app.post('/upload/:deviceID/:timestamp', multipartMiddleware, async ( request, r
     } else {
 
       images[request.params.deviceID] = data;
-
-      response.send();
       serverEmitter.emit('imageRefresh', { deviceID: request.params.deviceID, timestamp: request.params.timestamp });
+
+      let imageDate = new Date( Number( request.params.timestamp ) * 1000 );
+
+      let deviceID = request.params.deviceID;
+      let year = imageDate.getUTCFullYear().toString().padStart(4,'0');
+      let subyear = imageDate.getUTCMonth().toString().padStart(2,'0') + imageDate.getUTCDate().toString().padStart(2,'0') + imageDate.getUTCHours().toString().padStart(2,'0');
+      
+      let fileName = imageDate.getUTCMinutes().toString().padStart(2,'0') + imageDate.getUTCSeconds().toString().padStart(2,'0') + '.jpg';
+
+      await checkDirectoryStructure( __dirname + '/imageCache', deviceID, year, subyear );
+
+      let filepath = [ __dirname, 'imageCache', deviceID, year, subyear, fileName ].join('/');
+
+      fs.writeFile( filepath, data, ( error ) => {
+        if( error ) {
+          response.status(500);
+        }
+        response.send();
+      })
+
     }
 
   });
 });
+
+async function checkDirectoryStructure( ...dirs ) {
+
+  let base = '';
+  
+  dirs.forEach( async dir => {
+    base = base === '' ? dir : [ base, dir ].join('/');
+    await checkDirectory( base );
+  })
+
+}
+
+function checkDirectory( directory ) {
+  return new Promise( ( resolve, reject ) => {
+
+    fs.stat( directory, ( error, stat ) => {
+      if ( error ) {
+        reject();
+      }
+      if( !stat ) {
+        fs.mkdir( directory, ( error, d ) => {
+          resolve();
+        });
+      }
+      resolve();
+    } );
+
+  });
+}
 
 http.listen( 3000, function() {
   console.log( 'listening on port 3000' );
